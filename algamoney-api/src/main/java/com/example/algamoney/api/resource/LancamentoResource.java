c61f8a02a2801c5e556b777424e9a7f89304310a
americoafonso <americo.quim@hotmail.com>
2019-10-15 22:16:21 -0300

    implementation of findById method.

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -3,11 +3,14 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -16,9 +19,22 @@ public class LancamentoResource {
     @Autowired
     private LancamentoRepository lancamentoRepository;
 
+    /**
+     * Metodo para listar todos os lancamentos
+     *
+     * @return
+     */
     @GetMapping
     public List<Lancamento> listar() {
         return lancamentoRepository.findAll();
     }
+
+    @GetMapping("/{codigo}")
+    private ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
+        Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
+        return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+    }
+
+
 
 }
