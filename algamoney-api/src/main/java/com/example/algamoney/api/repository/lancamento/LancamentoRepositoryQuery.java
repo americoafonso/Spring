9212db15060749e24c0bd966782f9e1ab4ff3bcd
americoafonso <americo.quim@hotmail.com>
2019-12-07 21:59:41 -0300

    Paging Implementation.

--- algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,6 +2,8 @@ package com.example.algamoney.api.repository.lancamento;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.criteria.Root;
 import java.util.List;
@@ -9,5 +11,5 @@ import java.util.List;
 public interface LancamentoRepositoryQuery {
     public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Root<Lancamento> root);
 
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
